326d41bd6b8bc0134135477da41c48254815aa45
om-dev007 <officialomnarayan@gmail.com>
2026-03-11 01:18:50 +0530

    responsive page

--- Frontend/src/components/UrlForm.jsx
+++ Frontend/src/components/UrlForm.jsx
@@ -23,31 +23,36 @@ const UrlForm = ({ setLastUrl }) => {
     } catch (err) {
       console.log(err);
     }
-
   };
 
   return (
 
-    <form
-      onSubmit={formHandler}
-      className="flex flex-col md:flex-row gap-3 max-w-xl mx-3"
-    >
+    <div className="w-full px-4 mt-6 flex justify-center">
 
-      <input
-        type="text"
-        placeholder="Paste your long URL here..."
-        value={inputVal}
-        onChange={(e) => setInputVal(e.target.value)}
-        className="flex-1 px-4 py-3 rounded-lg bg-slate-700 text-white outline-none"
-      />
-
-      <button
-        className="bg-blue-600 cursor-pointer hover:bg-blue-700 px-6 py-3 rounded-lg text-white font-semibold"
+      <form
+        onSubmit={formHandler}
+        className="flex flex-col sm:flex-row gap-3 w-full max-w-2xl"
       >
-        Shorten
-      </button>
 
-    </form>
+        <input
+          type="text"
+          placeholder="Paste your long URL here..."
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+          className="flex-1 px-4 py-3 rounded-lg bg-slate-700 text-white outline-none 
+          focus:ring-2 focus:ring-blue-500 transition"
+        />
+
+        <button
+          className="bg-blue-600 cursor-pointer hover:bg-blue-700 px-6 py-3 rounded-lg 
+          text-white font-semibold transition"
+        >
+          Shorten
+        </button>
+
+      </form>
+
+    </div>
 
   );
 };
