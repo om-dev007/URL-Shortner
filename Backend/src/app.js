f2809175d87ae242eabeb71c82c7cfdf2f3a2e9d
om-dev007 <officialomnarayan@gmail.com>
2026-04-16 00:47:47 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,12 +4,7 @@ import urlRoutes from "./router/urlShorten.routes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://trimly-in.vercel.app"
-  ],
-}));
+app.use(cors());
 
 app.use(express.json());
 
