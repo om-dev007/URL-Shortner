43ba96f51b4c583dc7cd63f09b40baeb8e082485
om-dev007 <officialomnarayan@gmail.com>
2026-04-16 00:37:48 +0530

    backend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,22 +1,18 @@
-import e from "express";
+import express from "express";
+import cors from "cors";
 import urlRoutes from "./router/urlShorten.routes.js";
-import cors from 'cors';
 
-const app = e();
+const app = express();
 
 app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://trimly-in.vercel.app"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
 }));
-app.use(e.json());
+
+app.use(express.json());
+
 app.use("/api", urlRoutes);
 
-app.get("/", (req, res) => {
-    res.send("Welcome to Trimly URL Shortener API");
-});
-
-export default app
+export default app;
