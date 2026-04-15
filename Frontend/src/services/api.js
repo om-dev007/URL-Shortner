6936b7349a7c26083cb4255b759f689818b7d856
om-dev007 <officialomnarayan@gmail.com>
2026-04-15 14:47:39 +0530

    api added

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://url-shortner-h2wp.onrender.com",
+    baseURL: "https://trimly-back.vercel.app/",
     withCredentials: false
 })
 
